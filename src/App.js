4c4554299b1e555f7d443e2eb7fc76a55588827b
D.Loman <donnaloman@gmail.com>
2022-02-05 20:16:23 +0000

    created pages, started basic layout in eventPage, written tableExample(notworkingyet)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import "./App.css";
 import { Link } from "react-router-dom";
 
 function App() {
+    const columns = React.useMemo(
+        () => [
+          {
+            Header: "Company",
+            accessor: "company" // accessor is the "key" in the data
+          },
+          {
+            Header: "Contact",
+            accessor: "contact"
+          },
+          {
+            Header: "Country",
+            accessor: "country"
+          }
+        ],
+        []
+      );
+    const data = React.useMemo(
+        () => [
+          {
+            company: "Alfred",
+            contact: "Maria Anders",
+            country: "Germany"
+          },
+          {
+            company: "Centro comercial Moctezuma",
+            contact: "Francisco Chang",
+            country: "Mexico"
+          },
+          {
+            company: "Ernst Handel",
+            contact: "Roland Mendel	",
+            country: "Austria"
+          }
+        ],
+        []
+      );
 
 }
 
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import "../index.css";
+import "./cssPages/events.css"
 
-function Events() {
+
+
+function Events(columns) {
     return (
-        <div>
-            <h1>Events</h1>
+        <div className="wrapper">
+            <div className="pageTitle">
+            <h1>EVENTS</h1>
+            <div className="tablebox">
+                <div className="ufoReportsHeader">
+                <h1>table</h1>
+                
+
+                </div>
+
+                </div>
+        
+
+            </div>
         </div>
     )
 }
 
 export default Events
+
+
+
+/*
+<table>
+        <thead>
+          <tr>
+            <th>Description </th>
+            <th> Date </th>
+            <th> Location </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr> 
+            <td>target.id.description </td>
+            <td>target.id.date </td>
+            <td>target.id.location </td>
+          </tr>
+        </tbody>
+      </table>
+
+*/
