55c47a6eab68e1be15b1803d9537b1a73ef249eb
D.Loman <donnaloman@gmail.com>
2022-02-07 23:36:52 +0000

    css table layout simple

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Home } from "./pages/home";
 import { Events } from "./pages/events";
 import { Dates } from "./pages/dates";
 import { Locations } from "./pages/locations";
-import { NavBar } from "./components/navBar"
+import { NavBar } from "./components/navBar";
 import { LoginModal } from "./components/modal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
@@ -15,27 +15,24 @@ function App() {
 
   return (
     <div>
-    <BrowserRouter>
-      <NavBar 
-        open={open} 
-        setOpen={setOpen}
-      />
-      <LoginModal 
-        open={open} 
-        setOpen={setOpen} 
-        user= {user}
-        setUser={setUser}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dates" element={<Dates/>}/>
-        <Route path="/locations" element={<Locations/>}/>
-        <Route path="/events" element={<Events/>}/>
-        <Route path="/report" element={<Report user={user}/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar open={open} setOpen={setOpen} />
+        <LoginModal
+          open={open}
+          setOpen={setOpen}
+          user={user}
+          setUser={setUser}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dates" element={<Dates />} />
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/report" element={<Report user={user} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-  }
+  );
+}
 
 export default App;
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,63 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import "../App.css";
 // import "./cssPages/events.css"
 //button:
-import "../components/css/buttons.css"
+import "../components/css/buttons.css";
 // import Navbar from '../components/navBar'
+import PaginationTable from "../components/PaginationTable";
 
 export function Events() {
-    return (
-        <div className="wrapper">
-            <div className="pageTitle">
-            <h1>EVENTS</h1>
-            <div className="tablebox">
-                <div className="ufoReportsHeader">
-                  <h2>U.F.O REPORT</h2>
-                  <div className="table">
-                  <div class="btn close-btn"></div>
-                  <div class="btn delete-btn"></div>
-                  <div class="btn dlteX-btn"></div>
-                  <div class="btn add-btn"></div>
-                  <div><button class="btn edit-btn">EDIT</button></div>
-                  <div> <button class="btn add-button">ADD</button></div>
-                  <div><button class="btn delete-button">DELETE</button></div>
-                  <div><button class="btn return-button">RETURN</button></div>
-                  <div class="btn minus-btn"></div>
-                  
+  const [cells, setCells] = useState([]);
 
-                  </div>
-               
-                
+  const getData = async () => {
+    const res = await fetch(`${process.env.REACT_APP_BASE_URL}/report`, {
+      node: "cors",
+      method: "GET",
+    });
+    const data = await res.json();
+    const finalData = await data.data;
+    setCells(finalData);
+  };
 
-                </div>
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Description",
+        accessor: "description", // accessor is the "key" in the data
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+      },
+      {
+        Header: "Location",
+        accessor: "location",
+      },
+    ],
+    []
+  );
 
-                </div>
-        
+  useEffect(() => {
+    getData();
+  }, []);
 
+  const data = React.useMemo(() => cells, [cells]);
+
+  console.log(cells);
+  console.log(data);
+  return (
+    <div className="wrapper">
+      <div className="pageTitle">
+        <h1>EVENTS</h1>
+        <div className="tablebox">
+          <div className="ufoReportsHeader">
+            <h2>U.F.O REPORT</h2>
+            <div className="table">
+              <div>
+                <table>
+                  <thead></thead>
+
+                  <tbody>
+                    <td>
+                      <div>
+                        {cells && (
+                          <PaginationTable columns={columns} data={data} />
+                        )}
+                      </div>
+                      ;
+                    </td>
+                    {/* {cells.map(cells => (
+          <tr key={cells.id}>
+            
+          
+          
+          </tr>
+        ))} */}
+                  </tbody>
+                </table>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
-
-
-
-
-/*
-<table>
-        <thead>
-          <tr>
-            <th>Description </th>
-            <th> Date </th>
-            <th> Location </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr> 
-            <td>target.id.description </td>
-            <td>target.id.date </td>
-            <td>target.id.location </td>
-          </tr>
-        </tbody>
-      </table>
-*/
