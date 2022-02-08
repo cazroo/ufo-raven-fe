63175cf02db4f4ebd2bbcdf64b56acc38de1ad58
joe Pringle <joe-pringle@outlook.com>
2022-02-08 11:16:37 +0000

    netify compat 4

--- src/components/report.js
+++ src/components/report.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+// // import React from "react";
+import PaginationTable from "./PaginationTable";
 import "./css/report.css";
 
 function Report({ user }) {
@@ -7,6 +9,12 @@ function Report({ user }) {
   const [description, setDescription] = useState("");
   const [newReports, setNewReports] = useState([]);
   const [cells, setCells] = useState([]);
+
+  // const getData = async () => {
+  //   const resp = await fetch(`${process.env.REACT_APP_BASE_URL}/report`);
+  //   const data = await resp.json();
+  //   await setCell(data);
+  // };
 
   const handleDate = (e) => setDate(e.target.value);
   const handleLocation = (e) => setLocation(e.target.value);
@@ -45,7 +53,13 @@ function Report({ user }) {
     setCells(finalData);
   };
 
-
+  // const getData = async () => {
+  //   const resp = await fetch(`${process.env.REACT_APP_BASE_URL}/report`);
+  //   const data = await resp.json();
+  //   console.log(data)
+  //   setCell(data);
+  //   console.log(cell)
+  // };
 
   const columns = React.useMemo(
     () => [
@@ -76,59 +90,79 @@ function Report({ user }) {
   return (
     // <>{!user || user ? "" :
     <div className="App">
-      <div>
-        <div className="wrapper">
-          <div className="pageTitle">
-            <h1 className="pageTitle">REPORT MANAGEMENT</h1>
-            <div className="tablebox">
-              <div className="ufoReportsHeader">
-              <h1 className="pageTitle">Add New Report</h1>
-              <p className="report">{newReports}</p>
-              <form onSubmit={submitForm} className="test">
-                <label htmlFor="date" className="form">
-                  Date:{" "}
-                </label>
-                <input
-                  type="datetime-local"
-                  name="date"
-                  value={date}
-                  onChange={handleDate}
-                  required={true}
-                ></input>
+      <div className="report">
+        <h1>Report Management</h1>
+        <p className="report">{newReports}</p>
+        <form onSubmit={submitForm}>
+          <label htmlFor="date" className="form">
+            Date:{" "}
+          </label>
+          <input
+            type="datetime-local"
+            name="date"
+            value={date}
+            onChange={handleDate}
+            required={true}
+          ></input>
 
-                <label htmlFor="location" className="form">
-                  Location:{" "}
-                </label>
-                <input
-                  type="text"
-                  name="location"
-                  value={location}
-                  onChange={handleLocation}
-                  required={true}
-                  placeholder="Enter a location"
-                ></input>
-                <label htmlFor="description" className="form">
-                  Description:{" "}
-                </label>
-                <input
-                  type="text"
-                  name="description"
-                  value={description}
-                  onChange={handleDescription}
-                  required={true}
-                  placeholder="Please write a description"
-                ></input>
-                <input
-                  style={{ marginBottom: "4rem" }}
-                  type="submit"
-                  value="Add report"
-                  className="submitbtn"
-                ></input>
-              </form>
-              </div>
-            </div>
-          </div>
+          <label htmlFor="location" className="form">
+            Location:{" "}
+          </label>
+          <input
+            type="text"
+            name="location"
+            value={location}
+            onChange={handleLocation}
+            required={true}
+            placeholder="Enter a location"
+          ></input>
+          <label htmlFor="description" className="form">
+            Description:{" "}
+          </label>
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleDescription}
+            required={true}
+            placeholder="Please write a description"
+          ></input>
+          <input
+            style={{ marginBottom: "4rem" }}
+            type="submit"
+            value="Add report"
+            className="submitbtn"
+          ></input>
+        </form>
+
+
+
+        <div>
+
         </div>
+        <table>
+          <thead>
+            {/* <tr>
+              <th>Description </th>
+              <th> Date </th>
+              <th> Location </th>
+            </tr> */}
+          </thead>
+
+          
+          <tbody>
+            <td>
+            <div>{cells && <PaginationTable columns={columns} data={data} />}</div>;
+            </td>
+          {/* {cells.map(cells => (
+          <tr key={cells.id}>
+            
+          
+          
+          </tr>
+        ))} */}
+          </tbody>
+        </table>
       </div>
     </div>
     // }</>
@@ -136,3 +170,46 @@ function Report({ user }) {
 }
 
 export default Report;
+
+// const getAllReportsFetch = async () => {
+//     try {
+//         const res = await fetch(`${process.env.REACT_APP_BASE_URL}/report`, {
+//             node: "cors",
+//             method: "GET",
+//         })
+//         return await res.json();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// const getSingleReportFetch = async (id) => {
+//     try {
+//         const url = `${process.env.REACT_APP_BASE_URL}/report/${id}`;
+//         const res = await fetch (url, {
+//             method: "GET"
+//         });
+//         return await res.json();
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
+
+// const addReportFetch = async (title, date, location, description) => {
+//     try {
+//         const res = await fetch (`${process.env.REACT_APP_BASE_URL}report`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 title,
+//                 date,
+//                 location,
+//                 description
+//             })
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-
+// import "./cssPages/events.css"
+//button:
 import "../components/css/buttons.css"
+// import Navbar from '../components/navBar'
 import PaginationTable from "../components/PaginationTable";
 
 
@@ -47,25 +49,79 @@ export function Events() {
   console.log(data);
     return (
         <div className="wrapper">
-          <div className="pageTitle">
-            <h1 className="pageTitle">EVENTS</h1>
+            <div className="pageTitle">
+            <h1>EVENTS</h1>
             <div className="tablebox">
                 <div className="ufoReportsHeader">
                   <h2>U.F.O REPORT</h2>
                   <div className="table">
-                  <div className="btn close-btn"/>
-                  <div className="btn delete-btn"/>
-                  <div className="btn dlteX-btn"/>
-                  <div className="btn add-btn"/>
-                  <div><button className="btn edit-btn">EDIT</button></div>
-                  <div> <button className="btn add-button">ADD</button></div>
-                  <div><button className="btn delete-button">DELETE</button></div>
-                  <div><button className="btn return-button">RETURN</button></div>
-                  <div className="btn minus-btn"/>
+                  <div class="btn close-btn"></div>
+                  <div class="btn delete-btn"></div>
+                  <div class="btn dlteX-btn"></div>
+                  <div class="btn add-btn"></div>
+                  <div><button class="btn edit-btn">EDIT</button></div>
+                  <div> <button class="btn add-button">ADD</button></div>
+                  <div><button class="btn delete-button">DELETE</button></div>
+                  <div><button class="btn return-button">RETURN</button></div>
+                  <div class="btn minus-btn"></div>
+                  
+                  <div>
+                  <table>
+          <thead>
+            {/* <tr>
+              <th>Description </th>
+              <th> Date </th>
+              <th> Location </th>
+            </tr> */}
+          </thead>
+
+          
+          <tbody>
+            <td>
+            <div>{cells && <PaginationTable columns={columns} data={data} />}</div>;
+            </td>
+          {/* {cells.map(cells => (
+          <tr key={cells.id}>
+            
+          
+          
+          </tr>
+        ))} */}
+          </tbody>
+        </table>
+                  </div>
+                  </div>
+                
+
                 </div>
-              </div>
+
+                </div>
+        
+
             </div>
-          </div>
         </div>
     )
 }
+
+
+
+
+
+/*
+<table>
+        <thead>
+          <tr>
+            <th>Description </th>
+            <th> Date </th>
+            <th> Location </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr> 
+            <td>target.id.description </td>
+            <td>target.id.date </td>
+            <td>target.id.location </td>
+          </tr>
+        </tbody>
+      </table>
+*/
