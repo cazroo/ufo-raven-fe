8c2343b07b34e8067a7e2dbefbaf4b6c6c99ef95
joe Pringle <joe-pringle@outlook.com>
2022-02-10 17:05:38 +0000

    made navbar and footer persistent

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -12,44 +12,47 @@ export const NavBar = (props) => {
     }
 
     return (
-        <nav>
-            <div className="navbar">
-                <li className="nav-links">
-                    <div>
-                        <Link to="/">
-                            {/* <image src={requestAnimationFrame('../../Images/logo.svg')} /> */}
-                        </Link>
-                        <Link to="/events">
-                            <button className="navButton"> EVENTS </button>
-                        </Link>
-                        <Link to="/dates">
-                            <button className="navButton"> DATES </button>
-                        </Link>
-                        <Link to="/locations">                            
-                            <button className="navButton"> LOCATIONS </button>
-                        </Link>
-                    </div>
-                  
+        <div>
+            <div id="pageBuffer"/>
+            <nav>
+                <div className="navbar">
+                    <li className="nav-links">
+                        <div>
+                            <Link to="/">
+                                {/* <image src={requestAnimationFrame('../../Images/logo.svg')} /> */}
+                            </Link>
+                            <Link to="/events">
+                                <button className="navButton"> EVENTS </button>
+                            </Link>
+                            <Link to="/dates">
+                                <button className="navButton"> DATES </button>
+                            </Link>
+                            <Link to="/locations">                            
+                                <button className="navButton"> LOCATIONS </button>
+                            </Link>
+                        </div>
+                    
 
-                        {props.user ? 
-                        console.log(props.user.username) : null}
-                        {props.user ?
-                            <div>
-                                <Link to="/report">
-                                    <button className="navButton"> REPORT MANAGEMENT </button>
-                                </Link>
-                                <button className="navButton" onClick={() => logout()}>LOGOUT</button>
-                            </div>
-                        :
-                            <button className="navButton" onClick={() => props.setOpen(true)}>
-                                Login or Register
-                            </button>
-                        }
-                      
-                   
-                </li>
-            </div>
-        </nav>
+                            {props.user ? 
+                            console.log(props.user.username) : null}
+                            {props.user ?
+                                <div>
+                                    <Link to="/report">
+                                        <button className="navButton"> REPORT MANAGEMENT </button>
+                                    </Link>
+                                    <button className="navButton" onClick={() => logout()}>LOGOUT</button>
+                                </div>
+                            :
+                                <button className="navButton" onClick={() => props.setOpen(true)}>
+                                    Login or Register
+                                </button>
+                            }
+                        
+                    
+                    </li>
+                </div>
+            </nav>
+        </div>
     )
 }
 
